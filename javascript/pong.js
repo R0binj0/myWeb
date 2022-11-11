14495e5cdef4d1372a913977961c95fd3fbd2f30
R0binj0 <robin.pruunlep@ametikool.ee>
2022-11-11 14:01:38 +0200

    commit

--- javascript/pong.js
+++ javascript/pong.js
@@ -1,4 +1,9 @@
-var pressCount = 0
+var canvas = document.querySelector("canvas");
+var l = canvas.getContext('2d');
+var x = Math.floor(Math.random() * innerWidth);
+var y = Math.floor(Math.random() * innerHeight);
+var vx = Math.floor(Math.random() * 20);
+var vy = Math.floor(Math.random() * 4);
 
 document.onkeydown = function(event) {
     switch (event.keyCode) {
@@ -16,3 +21,33 @@ document.onkeydown = function(event) {
             break;
     }
 };
+
+function move() {
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    requestAnimationFrame(move);
+
+    l.clearRect(0, 0, innerWidth, innerHeight);
+    l.beginPath();
+    l.strokeStyle = "white";
+    l.arc(x, y, 20, 0, Math.PI * 2, false);
+    l.stroke();
+    
+    if (20 + x > innerWidth)
+        vx = 0 - vx;
+
+    if (x - 20 < 0)
+        vx = 0 - vx;
+
+    if (y + 20 > innerHeight)
+        vy = 0 - vy;
+
+    if (y - 20 < 0)
+        vy = 0 - vy;
+
+    x = x + vx;
+    y = y + vy;
+
+}
+move();
