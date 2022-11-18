caaa95967b5695fd75f1bccb2a7c054b06f3128f
R0binj0 <robin.pruunlep@ametikool.ee>
2022-11-18 11:08:31 +0200

    commit

--- javascript/pong.js
+++ javascript/pong.js
@@ -1,10 +1,6 @@
 const player1 = document.getElementById("player1");
 const player2 = document.getElementById("player2");
 const ball = document.getElementById("ball")
-var x = Math.floor(Math.random() * 500);
-var y = Math.floor(Math.random() * 350);
-var vx = Math.floor(Math.random() * 20);
-var vy = Math.floor(Math.random() * 4);
 
 player1.style.top ="0px";
 player2.style.top ="0px";
@@ -33,7 +29,3 @@ document.onkeydown = function(event) {
             break;
     }
 };
-function move() {
-    
-}
-move()
